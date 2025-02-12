39019ef83e5ca70fb41a683ddce7e92fa4b881f7
WillianMoreiraBFP <162322947+WillianMoreiraBFP@users.noreply.github.com>
2025-02-12 16:33:24 -0300

    Initial commit

---
+++ Back-end/src/main/java/com/alura/screemmatch/frases/ScreemMatchFrasesApplication.java
@@ -0,0 +1,13 @@
+package com.alura.screemmatch.frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreemMatchFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreemMatchFrasesApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/alura/screemmatch/frases/ScreemMatchFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.screemmatch.frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreemMatchFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
